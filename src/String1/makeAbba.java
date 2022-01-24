11c1a09e1e5d8cb2041a184483d258c99d0a4101
Max Manas Kalenov <manasworks@gmail.com>
2022-01-24 14:13:43 -0600

    Question 2 Strings

---
+++ src/String1/makeAbba.java
@@ -0,0 +1,20 @@
+package String1;
+
+public class makeAbba {
+    public static void main(String[] args) {
+
+    }
+
+//  public String makeAbba(String a, String b) {
+//
+//  }
+}
+/*
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+
+makeAbba("Hi", "Bye") → "HiByeByeHi"
+makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+makeAbba("What", "Up") → "WhatUpUpWhat"
+
+ */
