6dbd078de2e808047a8b13358923660eefa8efd8
Max Manas Kalenov <manasworks@gmail.com>
2022-01-24 14:10:04 -0600

    First Question

---
+++ src/String1/helloName.java
@@ -0,0 +1,19 @@
+package String1;
+
+public class helloName {
+    public static void main(String[] args) {
+       // System.out.println(helloName("Your name"));
+    }
+
+//    public String helloName(String name) {
+//
+//    }
+}
+
+/*
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+ */
